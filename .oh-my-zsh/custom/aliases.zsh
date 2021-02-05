25eea7ae263e5f873cc4a6c91bafe12961c4d14a
Martin Matyasek <martin.matyasek@gmail.com>
2021-02-05 15:48:21 +0100

    Add aliases for copy/paste using xclip

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,6 +5,11 @@ alias zshconfig="editor ~/.zshrc"
 # XDG
 alias o="xdg-open"
 
+# Copy/Paste
+alias y="xclip -i -selection clipboard"
+alias c="xclip -i -selection clipboard"
+alias p="xclip -o -selection clipboard"
+
 # EDITOR
 alias e="nvim"
 alias v="nvim"
