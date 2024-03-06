a4212ac4061732e5e3377e2fe854a5d4004e5a5e
Martin Matyášek <martin.matyasek@gmail.com>
2024-03-06 09:53:09 +0100

    Rename `init_ubuntu.sh` to `bootstrap.sh`
    Don't forget to run `make links` and remove the old symlink by running
    `rm $XDG_BIN_HOME/init_ubuntu.sh`.

--- .local/bin/bootstrap.sh
+++ .local/bin/bootstrap.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo ">>> Running initial upgrade..." 
+echo ">>> Running initial upgrade..."
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
 
-echo ">>> Installing basic tools..." 
+echo ">>> Installing basic tools..."
 sudo apt install -y make git
 
 echo ">>> Creating custom directories..."
 mkdir -p ~/dev ~/tmp
-
