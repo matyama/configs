a19b918c33197ea3299b3d6f8c316c9392f7552e
Martin Matyášek <martin.matyasek@gmail.com>
2024-06-23 11:09:28 +0200

    Replace `neoformat` with Lua-based `conform.nvim`
    This commit switches code formatting plugin for a Lua-based
    implementation. That is it replaces `neoformat` with `conform.nvim`.
    
    Moreover, the `sql.vim` auto-load script is no longer necessary, so the
    whole `autoload` directory can now be dropped.

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,24 +1,56 @@
--- TODO: deprecate in favor of `conform.nvim` (use `format_on_save`)
--- A (Neo)Vim plugin for formatting code (https://github.com/sbdchd/neoformat)
+-- Lightweight yet powerful formatter plugin for Neovim
+-- https://github.com/stevearc/conform.nvim
 return {
-  "sbdchd/neoformat",
-  config = function()
-    local g = vim.g
-    local api = vim.api
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
 
-    -- Shell (shfmt): indent switch cases
-    g.shfmt_opt = "-ci"
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
 
-    -- Hotfix: disables formatting zsh files, because shfmt breaks:
-    -- `(( $+commands[x] ))`
-    g.neoformat_enabled_zsh = {}
+  opts = {
+    -- Define formatters (use a sub-list to run only the first available)
+    formatters_by_ft = {
+      bash = { "shfmt" },
+      lua = { "stylua" },
+      python = function(bufnr)
+        if require("conform").get_formatter_info("ruff_format", bufnr).available then
+          return { "ruff_format" }
+        else
+          return { "isort", "black" }
+        end
+      end,
+      sql = { "sqlfluff" },
+      sh = { "shfmt" },
+    },
 
-    -- Run a formatter on save
-    local fmt = api.nvim_create_augroup("fmt", { clear = true })
-    api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      group = fmt,
-      command = "undojoin | Neoformat",
-    })
+    -- Set up format-on-save
+    format_on_save = function(bufnr)
+      -- sqlfluff takes too long, so increase the timeout
+      if require("conform").get_formatter_info("sqlfluff", bufnr).available then
+        return { timeout_ms = 1000, lsp_format = "fallback" }
+      end
+
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        prepend_args = { "-ci" },
+      },
+    },
+  },
+
+  init = function()
+    vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
   end,
 }
