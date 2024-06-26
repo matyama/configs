1a1be6632cae86c92910f621c43d50867c7aa121
Martin Matyášek <martin.matyasek@gmail.com>
2024-06-26 10:22:34 +0200

    Apply `proximity-sort` when searching for `:Files`

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -61,33 +61,25 @@ return {
       },
     })
 
-    --    -- When using :Files, pass the file list through
-    --    --
-    --    --   https://github.com/jonhoo/proximity-sort
-    --    --
-    --    -- to prefer files closer to the current file.
-    --    local function list_cmd()
-    --      local fn = vim.fn
-    --      local base = fn.fnamemodify(fn.expand("%"), ":h:.:S")
-    --      if base == "." then
-    --        -- proximity-sort won't work if there's no current file
-    --        return "fd --type file --follow --hidden"
-    --      else
-    --        return fn.printf("fd --type file --follow --hidden | proximity-sort %s", fn.shellescape(fn.expand("%")))
-    --      end
-    --    end
+    -- When using :Files, pass the file list through
     --
-    --    local function files(arg)
-    --      vim.fn["fzf#vim#files"](arg.qargs, { source = list_cmd(), options = "--tiebreak=index" }, arg.bang)
-    --    end
-
-    -- TODO: https://github.com/jonhoo/proximity-sort
-    -- see: https://github.com/ibhagwan/fzf-lua/wiki/Advanced
-    local function files(arg)
-      return require("fzf-lua").files()
+    --   https://github.com/jonhoo/proximity-sort
+    --
+    -- to prefer files closer to the current file.
+    local function list_cmd()
+      local fn = vim.fn
+      local file = fn.expand("%")
+      if fn.fnamemodify(file, ":h:.:S") == "." then
+        -- proximity-sort won't work if there's no current file
+        return "fd --type file --follow --hidden"
+      else
+        return fn.printf("fd --type file --follow --hidden | proximity-sort %s", fn.shellescape(file))
+      end
     end
 
-    vim.api.nvim_create_user_command("Files", files, {
+    vim.api.nvim_create_user_command("Files", function()
+      return require("fzf-lua").files({ cmd = list_cmd() })
+    end, {
       bang = true,
       nargs = "?",
       complete = "dir",
