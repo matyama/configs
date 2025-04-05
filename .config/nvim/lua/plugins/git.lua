fd237131e9464b956e3ef11f1500b0f0c5f592aa
Martin Matyášek <martin.matyasek@gmail.com>
2025-04-05 16:37:59 +0200

    Add `blame.nvim` plugin

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -28,4 +28,23 @@ return {
       g.lazygit_floating_window_use_plenary = 0
     end,
   },
+
+  -- Neovim fugitive style git blame plugin
+  -- https://github.com/FabijanZulj/blame.nvim
+  {
+    {
+      "FabijanZulj/blame.nvim",
+      lazy = false,
+      keys = {
+        { "<leader>b", "<cmd>BlameToggle window<cr>", desc = "BlameToggle" },
+      },
+      config = function()
+        require("blame").setup({})
+      end,
+      opts = {
+        -- ignore whitespace when comparing
+        blame_options = { "-w" },
+      },
+    },
+  },
 }
